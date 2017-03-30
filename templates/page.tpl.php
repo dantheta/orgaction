ffcbcaaafabf8bf66b3d322a8ad00d8ca773d064
Daniel Ramsay <dr@circlesquares.co.uk>
2017-03-30 22:01:45 +0100

    reinstate original footer, place footer content in block

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -184,7 +184,7 @@
   <?php endif; ?>
 
 
-  <?php /* if($page['footer'] || $secondary_menu): ?>
+  <?php if($page['footer'] || $secondary_menu): ?>
 
   <footer id="footer" role="contentinfo">
 
@@ -209,98 +209,7 @@
 
   </footer><!-- /#footer -->
 
-  <?php endif; */ ?>
-  <div id="footer-wrapper">
-  <footer id="footer" role="contentinfo" class="middle">
-
-     <div class="two columns" style="
-	    clear: left;
-	">Who</div>
-		<div class="fourteen columns">
-			<p>Open Rights Group exists to preserve and promote your rights in the digital age. We are funded by thousands of people like you. We are based in London, United Kingdom. Open Rights is a non-profit company limited by Guarantee, registered in England
-				and Wales no. <a href="http://data.companieshouse.gov.uk/doc/company/05581537" rel="noreferrer">05581537</a>.</p>
-		</div>
-		<div class="two columns">Phone & Address</div>
-		<div class="fourteen columns">
-  <p><a href="tel:+442070961079">+442070961079</a>
-    <br>Unit 7, Tileyard Acorn Studios, 103-105, Blundell Street, London, N7 9BN</p>
-		</div>
-
-		<div class="two columns">Site</div>
-		<div class="fourteen columns footerLinks">
-			<p class="caps">
-				<a href="https://www.openrightsgroup.org/about/">About</a> /
-				<a href="https://www.openrightsgroup.org/jobs/">Jobs</a> /
-				<a href="https://www.openrightsgroup.org/contact/">Contact</a> /
-				<a href="https://www.openrightsgroup.org/press/">Press</a> /
-				<a href="https://www.openrightsgroup.org/privacy/">Privacy</a> /
-				<a href="https://www.openrightsgroup.org/licence/">Licence</a>
-			</p>
-		</div>
-
-		<div class="two columns">ORG</div>
-		<div class="fourteen columns">
-			<p class="caps">
-				<a href="https://www.openrightsgroup.org/join/">Join</a> /
-				<a href="https://www.openrightsgroup.org/donate/">Donate</a> /
-				<a href="https://www.openrightsgroup.org/blog/">Blog</a> /
-				<a href="https://openrightsgroup.spreadshirt.net/" rel="noreferrer">Shop</a> / <a href="https://www.openrightsgroup.org/press/">Press</a> / <a href="https://www.openrightsgroup.org/events/">Events</a> /
-				<a href="https://wiki.openrightsgroup.org/wiki/Main_Page">Wiki</a>
-			</p>
-		</div>
-
-		<div class="two columns">Issues</div>
-		<div class="fourteen columns">
-			<p class="caps">
-				<a href="https://www.openrightsgroup.org/issues/gchq">Mass Surveillance</a> / <a href="https://www.openrightsgroup.org/issues/censorship">Censorship</a> / <a href="https://www.openrightsgroup.org/issues/copyright-reform">Copyright reform</a> /
-				<a
-				href="https://www.openrightsgroup.org/issues/data-protection">Data protection</a> / <a href="https://www.openrightsgroup.org/issues/opendata">Open Data</a> / <a href="https://www.openrightsgroup.org/issues/open-standards">Open Standards</a>
-			</p>
-		</div>
-
-
-		<div class="two columns">Connect</div>
-		<div class="fourteen columns connect">
-			<a href="https://www.facebook.com/openrightsgroup">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_facebook.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://twitter.com/openrightsgroup">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_twitter.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://plus.google.com/u/0/116543318055985390327/posts">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_googleplus.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://www.flickr.com/photos/tags/openrightsgroup/">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_share.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://www.youtube.com/openrightsgroup">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_youtube.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://www.linkedin.com/groups/Open-Rights-Group-133233">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_linkedin.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://www.meetup.com/ORG-London/">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_red.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://www.openrightsgroup.org/feed/">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_rss.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://soundcloud.com/openrightsgroup">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_cloud.png" alt="" title="" width="30" height="29">
-			</a>
-			<a href="https://openrightsgroup.tumblr.com">
-				<img src="https://www.openrightsgroup.org/assets/site/org/images/social_tumblr.png" alt="" title="Tumblr" width="30" height="29">
-			</a>
-		</div>
-
-		<div class="sixteen columns right copyright">
-			<p>© 2005 - 2016, free to reuse except where stated. <a href="/licence">Credits</a></p>
-			<img src="https://www.openrightsgroup.org/assets/site/org/images/ccBadge.jpg" class="ccBadge">
-		</div>
-
-
-  </footer>
-  </div>
+  <?php endif;  ?>
 
   </div><!-- /#page -->
 </div><!-- /#page-wrapper -->
